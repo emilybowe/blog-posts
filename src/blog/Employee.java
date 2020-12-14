65a1ca2463df562432c8e162668d6e622e1240f2
Emily Bowe <ebowe@tcd.ie>
2020-12-14 20:30:32 +0000

    Fields, getters/setters, constructors

--- src/blog/Employee.java
+++ src/blog/Employee.java
@@ -1,5 +1,45 @@
 package blog;
 
 public class Employee {
+	private String name;
+	private int age;
+	private String location;
+	
+	Employee(String name, int age, String location) {
+		name = this.getName();
+		age = this.getAge();
+		location = this.getLocation();
+	}
+	
+	Employee() {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
 
 }
+
+
+
---
+++ src/blog/Main.java
@@ -0,0 +1,10 @@
+package blog;
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+		
+	}
+
+}
